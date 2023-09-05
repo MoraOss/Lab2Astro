2a17bdbe106c67cf1ab0ab79d21850e5cda8a9a3
MoraOs <jamora3@uc.cl>
2023-09-04 23:07:59 -0300

    noche

--- abrirarchivos.py
+++ abrirarchivos.py
@@ -28,6 +28,27 @@ def dict_promedio(ruta_archivo, desde, hasta):
     
     return promedio_flats
 
+
+def mostrar_histograma_y_imagen(dato_a_mostrar, titulo_histograma, titulo_imagen):
+    # Genera un histograma de los valores de píxeles
+    hist_values, bin_edges = np.histogram(dato_a_mostrar.flatten(), bins='auto')
+
+    # Histograma
+    plt.hist(dato_a_mostrar.flatten(), bins=bin_edges, color='b', alpha=0.7, rwidth=0.85)
+    plt.xlabel('Valor del píxel')
+    plt.ylabel('Frecuencia')
+    plt.title(titulo_histograma)
+    plt.grid(axis='y', alpha=0.75)
+    plt.show()  # Muestra el histograma
+
+    # Muestra la imagen
+    plt.figure()
+    plt.imshow(dato_a_mostrar, cmap='jet')
+    plt.title(titulo_imagen)
+    plt.colorbar()
+    plt.show()  # Muestra la imagen
+
+
 ruta_flats = r"J:\UC\Segundo Semestre 2023\Laboratorio de Instrumentación Astronómica\Lab 2\Nueva carpeta\Flats 182 sec\Image0"
 promedio_flats = dict_promedio(ruta_flats, 1, 101)
 print("FLATS:", promedio_flats)
@@ -45,24 +66,8 @@ std_dev = np.std(promedio_dark)
 print("Desviación Estándar:", std_dev)
 
 
-# Genera un histograma de los valores de píxeles
-hist_values, bin_edges = np.histogram(promedio_dark.flatten(), bins='auto')
 
-# Histograma
-plt.hist(promedio_dark.flatten(), bins=bin_edges, color='b', alpha=0.7, rwidth=0.85)
-plt.xlabel('Valor del píxel')
-plt.ylabel('Frecuencia')
-plt.title('Histograma de Valores de Píxeles')
-plt.grid(axis='y', alpha=0.75)
-plt.show()  # Muestra el histograma
-
-# Muestra la imagen
-plt.figure()
-plt.imshow(promedio_dark, cmap='jet')
-plt.title('Imagen TIFF')
-plt.colorbar()
-plt.show()  # Muestra la imagen
-
-# Información sobre la forma de los datos (píxeles)
-print("Forma de los datos:", promedio_dark.shape)
+mostrar_histograma_y_imagen(promedio_dark, "DARK", "DARK1")
+mostrar_histograma_y_imagen(promedio_bias, "BIAS", "BIAS1")
+mostrar_histograma_y_imagen(promedio_flats, "FLAT", "FLAT1")
 
